865e55c852e34bc895ec38b36c8bc4a3933ed73c
Sebabin <sebastiancrespin279@gmail.com>
2022-02-19 00:26:49 -0700

    user can now log in and create a sesion if hashed password matches user input password, as well as initializing a session upon user creation

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const {User, Review, Post} = require('../../models');
+const bcrypt = require('bcrypt');
 
 // get all users
 router.get('/', (req,res) => {
@@ -50,11 +51,46 @@ router.post('/', (req,res) => {
         email: req.body.email,
         password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+        req.session.save(() => {
+         // declare session variables
+         req.session.user_id = dbUserData.id;
+         req.session.username = dbUserData.username;
+         req.session.loggedIn = true;
+         res.json(dbUserData)
+         console.log(req.session.username);
+        });
+    })
     .catch(err => {
         console.log(err);
         res.status(400).json(err);
     });
+});
+
+// login route
+router.post('/login',(req,res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if(!dbUserData) {
+            res.status(400).json({message: 'no user found with this id'});
+            return;
+        }
+       bcrypt.compare(req.body.password,dbUserData.password,function(err, result) {
+           if(result === false) {
+               res.status(400).json({message: 'incorrect password'});
+               return;
+           }
+           req.session.save(() => {
+               req.session.user_id = dbUserData.id;
+               req.session.username = dbUserData.username;
+               req.session.loggedIn = true;
+               res.json({user: dbUserData, message: 'you are now logged in!'});
+           });
+       });
+    })
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,33 @@
+// requiring native node.js path module
 const path = require('path');
 const express = require('express');
+// sequelize connection
 const sequelize = require('./config/connection');
+// requiring express.js
 const app = express();
+// configuring port
 const PORT = process.env.PORT || 3001;
+// getting all routes for use in as middlewear
 const routes = require('./controllers');
+// require express session this allows us to create sessions
+const session = require('express-session');
+// require cinnect session sequelize this connects express session and sequelize
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// require dotenv package to read our .env file for our mySecret in th sess{}
+require('dotenv').config();
+
+// make our session object
+const sess = {
+   secret: process.env.mySecret,
+   cookie:{},
+   resave: false,
+   saveUninitialized: true,
+   store: new SequelizeStore({
+       db:sequelize
+   })
+};
+// use the sess we created 
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -13,6 +37,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 // use our controller routes
 app.use(routes);
 
+// sync all of my sequelize models with the db for data creation
 sequelize.sync({force: false}).then(() => {
+    // turning on the server
     app.listen(PORT, () => console.log(`now listening 0n port:${PORT}`));
 });
