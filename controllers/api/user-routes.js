5721e025fd4a05d409c8410c3c4ab20c8650211a
Sebabin <sebastiancrespin279@gmail.com>
2022-02-19 20:45:22 -0700

    user CRUD routes complete

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -93,4 +93,58 @@ router.post('/login',(req,res) => {
     })
 });
 
+// logout 
+router.post('/logout', (req,res) => {
+    if(req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }else{
+        res.status(404).end();
+    }
+});
+
+// update user info
+router.put('/:id',(req,res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData[0]) {
+            res.status(404).json({message: 'No user found with this id'});
+            return;
+        }
+        res.json(dbUserData);
+        console.log(dbUserData);
+        console.log(dbUserData[0]);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+// delete user
+router.delete('/:id',(req,res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if(!dbUserData) {
+            res.status(404).json({message: 'No user found with this id!'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
