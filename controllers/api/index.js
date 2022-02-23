859913ed5632af970cbf7e4ece3e6caa4310323a
Sebabin <sebastiancrespin279@gmail.com>
2022-02-22 20:44:14 -0700

    have s3 set up as well as multer, along with a function to upload to s3 bucket

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,13 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
 const reviewRoutes = require('./review-routes');
+const leadRoutes = require('./lead-routes');
+
+// consume lead routes
+router.use('/leads', leadRoutes);
 
 // consume review routes
-router.use('/reviews',reviewRoutes);
+router.use('/reviews', reviewRoutes);
 
 // consume my postRoutes
 router.use('/posts', postRoutes);
--- controllers/api/lead-routes.js
+++ controllers/api/lead-routes.js
@@ -1,3 +1,14 @@
 const router = require('express').Router();
-const authorize_user = require('../../utils/autorize-user');
 const {Lead} = require('../../models');
+// import multer for uploading file through a form
+const multer = require('multer');
+// configure multer instance to put files in a directory called lead-uploads
+var video_upload = multer({dest: 'lead-uploads/'});
+// import function that uploads to the s3 bucket
+const {s3Upload} = require('../../utils/s3');
+
+router.post('/',video_upload.single('walk-throug-video'),async (req,res) => {
+    await s3Upload(req.file);
+});
+
+module.exports = router;
