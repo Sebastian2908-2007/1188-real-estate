214e0d76897485d0a16b13fb3b4ff02436c28fc4
Sebabin <sebastiancrespin279@gmail.com>
2022-02-25 18:41:33 -0700

    have videos uploading to  s3 as well as s3 key being stored in db lead table as walk_through_video

--- controllers/api/lead-routes.js
+++ controllers/api/lead-routes.js
@@ -7,12 +7,22 @@ var video_upload = multer({dest: 'lead-uploads/'});
 // import function that uploads to the s3 bucket
 const {s3Upload} = require('../../utils/s3');
 
+router.get('/',(req,res) => {
+    Lead.findAll()
+    .then(dbLeadData => res.json(dbLeadData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// this route will post a lead to the db and a video to amazon s3
 router.post('/',video_upload.single('walk-through-video'),async (req,res) => {
     const video_file = req.file;
     console.log(video_file);
     // use s3 function to upload file to s3
     const walk_through_video = await s3Upload(video_file);
-    console.log(walk_through_video.key);
+    console.log(walk_through_video.Key);
     
     /*const body = req.body
     console.log(body.first_name);
@@ -23,26 +33,24 @@ router.post('/',video_upload.single('walk-through-video'),async (req,res) => {
   
     console.log(body.address);*/
   
-
-    //const result = await s3Upload(req.file);
-    //console.log(result);
-    /*Lead.create({
+    Lead.create({
        first_name: req.body.first_name,
        last_name: req.body.last_name,
        email: req.body.email,
        address: req.body.address,
-       walk_through_video: req.file.Key
+       walk_through_video: walk_through_video.Key
     }).then(dbLeadData => {
         if(!dbLeadData) {
             res.status(500).json({message: 'incorrect data'});
             return;
         } 
+        console.log(dbLeadData);
         console.log(dbLeadData.walk_through_video); 
     })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
-    });*/
+    });
 });
 
 module.exports = router;
