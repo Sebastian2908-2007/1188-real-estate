820d0aa9d57620d42f355f6d9d3b93fae5a9150b
Sebabin <sebastiancrespin279@gmail.com>
2022-03-02 13:35:33 -0700

    added a footer and a header with navigation to the main.handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,12 @@
 const router = require('express').Router();
 
+// this will render the homepage
+router.get('/',(req,res) => {
+   res.render('homepage');
+});
+
+
+// this will render the lead form page
 router.get('/get-cash-offer',(req,res) => {
    res.render('lead-page');
 });
