8d6d06518be0d3bdd2621d4d963c3c145b9abc45
Sebabin <sebastiancrespin279@gmail.com>
2022-03-19 14:46:26 -0600

    added js media queries in admin-nav-footer.js for the deropdown section

--- public/javascript/admin-nav-footer.js
+++ public/javascript/admin-nav-footer.js
@@ -4,6 +4,12 @@ const path = window.location.pathname.split('/');
 // get dropdown container element
 const dropdown =  document.getElementById('dropdown');
 const dropDownContent = document.querySelector('#dropdown-content');
+// media query variables start
+const minWidth768 = window.matchMedia('(min-width: 768px)');
+const minWidth1024 = window.matchMedia('(min-width: 1024px)');
+const minWidth1280 = window.matchMedia('(min-width: 1280px)');
+const minWidth1366 = window.matchMedia('(min-width: 1366px)');
+const minWidth1920 = window.matchMedia('(min-width: 1920px)');
 
 
 function navList() {
@@ -19,11 +25,36 @@ function navList() {
     
       // padding that will work on dropdown content '18% 111% 1% 11%';
     
-    // change #dropdown to margin-right 20% when on admin side
-   dropdown.style.margin = '0 18% 0 0';
+    // change #dropdown to margin-right 20% when on admin side 18
+   dropdown.style.margin = '0 42% 0 0';
    dropDownContent.style.padding = '0%';
    navList.style.alignItems = 'center';
+
+     // media query nested if else if block starts
+
+  if(minWidth768.matches) {
+    dropdown.style.margin = '0 44% 0 0';
   }
+
+   if (minWidth1024.matches) {
+    dropdown.style.margin = '0 55% 0 0';
+  }
+
+   if (minWidth1280.matches) {
+    dropdown.style.margin = '0 65% 0 0';
+  }
+
+   if (minWidth1366.matches) {
+    dropdown.style.margin = '0 67% 0 0';
+  }
+
+   if (minWidth1920.matches) {
+    dropdown.style.margin = '0 72% 0 0';
+  }
+
+
+  }
+
     
    
 };
