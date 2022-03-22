5b3d13d859203f11be5b90b034a46179efb950fd
Sebabin <sebastiancrespin279@gmail.com>
2022-03-21 20:37:41 -0600

    lead submit validation working

--- public/javascript/add-lead.js
+++ public/javascript/add-lead.js
@@ -6,6 +6,15 @@ async function newLeadHandler(event) {
 
 const form = document.querySelector('#lead-submit-form');
 const formData = new FormData(form);
+// below grabs the p in lead form
+const confirmTextp = document.querySelector('#confirmtextp');
+// this div will house our wait mssg
+const waitMssg = document.createElement('div');
+ waitMssg.textContent = 'wait while we process ...';
+ waitMssg.style.fontSize = '3em';
+ // append wait mssg to the paragraph in the form
+ confirmTextp.appendChild(waitMssg);
+
  const response = await fetch('/api/leads',{
      method: 'POST',
      body: formData
