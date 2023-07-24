3f08665a1b490fc8edcae11a9df44c55fce29a0b
Sebabin <sebastiancrespin279@gmail.com>
2023-07-23 19:46:20 -0600

    added a few logo pics to play with

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -1,7 +1,15 @@
-
+import Image from "next/image";
+import logo from '../public/relogo (1).png';
 const Heading = () => {
     return(
-    <header className="bg-sitedrkblu">
+    <header className="bg-sitedrkblu flex flex-row justify-between">
+        <div>
+            <Image
+            className="logo"
+            src={logo}
+            alt="2908 realestate logo"
+            />
+        </div>
         <nav className="flex flex-row justify-evenly">
             <span className="text-sitegrn text-sm">Process</span>
             <span className="text-sitegrn text-sm">Reviews</span>
