2eefb032871a81db4cd8a73bf2ecb40c49c954cd
Sebabin <sebastiancrespin279@gmail.com>
2023-07-24 13:35:44 -0600

    added default mui drawer

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import logo from '../public/relogo (1).png';
+import DrawerNav from "./DrawerNav";
 const Heading = () => {
     return(
     <header className="bg-sitedrkblu flex flex-row justify-between">
@@ -10,12 +11,7 @@ const Heading = () => {
             alt="2908 realestate logo"
             />
         </div>
-        <nav className="flex flex-row justify-evenly">
-            <span className="text-sitegrn text-sm">Process</span>
-            <span className="text-sitegrn text-sm">Reviews</span>
-            <span className="text-sitegrn text-sm">About</span>
-            <span className="text-sitegrn text-sm">Sell now</span>
-        </nav>
+    <DrawerNav/>
     </header>
     );
 };
