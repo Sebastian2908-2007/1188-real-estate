62cdfe8071b36bcd9e6716195676728e10b4c997
Sebabin <sebastiancrespin279@gmail.com>
2023-07-25 20:37:33 -0600

    added 7 new pictures and put them on home page for the more than section and easiest way section

--- app/page.js
+++ app/page.js
@@ -5,6 +5,13 @@ import UglyHouse from '../public/homeugly-house1.jpg';
 import UglyHouse2 from '../public/homeugly-house2.jpg';
 import UglyHouse3 from '../public/homeugly-house3.jpg';
 import UglyHouse4 from '../public/homeugly-house4.jpg';
+import easiestPic2 from '../public/easiestway2.jpg';
+import easiestPic3 from '../public/easiestway3.jpg';
+import easiestPic4 from '../public/easiestway4.jpg';
+import moreThan1 from '../public/morethan1.jpg';
+import moreThan2 from '../public/morethan2.jpg';
+import moreThan3 from '../public/morethan3.jpg';
+/**easiestpic 1 can  be used elwhere in project*/
 export default function Home() {
   return (
     
@@ -12,12 +19,21 @@ export default function Home() {
    <Hero/>
    <section className="flex flex-col items-center mb-4 mt-2">
    <InitialForm/>
+  <section>
    <section className="p-4 w-64 bg-white rounded-lg mt-4">
-    <h2 className="text-xl text-center mb-4 font-bold">The easiest way to sell your house</h2>
+    <h2 className="text-xl text-center mb-4 font-bold">The easiest way to sell your house fast</h2>
+    <Image
+   src={easiestPic2}
+   alt="A watch or clock that conveys the speed at which 2908 real estate can by your house"
+   />
     <p className="text-center mb-2">
       At 2908 Real Estate, we are dedicated to ensuring that the process of selling
        your house in Utah is faster, easier, and stress-free.
     </p>
+    <Image
+   src={easiestPic4}
+   alt="A distressed house bought fast by 2908 real estate"
+   />
     <p className="text-center mb-2">
     If you are committed 
       to selling your property in UT, we stand prepared to present you with a fair, all-cash offer.
@@ -26,6 +42,10 @@ export default function Home() {
     What sets us apart is that we purchase your house directly from you, as-is, 
     allowing you to walk away without the burden of handling any repairs. 
     </p>
+    <Image
+   src={easiestPic3}
+   alt="A nice house bought quickly by 2908 real estate"
+   />
     <p className="text-center mb-2">
     Additionally, we take
      care of property clean-up for you, making the entire experience remarkably easy and convenient.
@@ -34,10 +54,18 @@ export default function Home() {
 
    <section className="p-4 w-64 bg-white rounded-lg mt-4">
    <h2 className="text-xl text-center mb-4 font-bold">More Than Cash Offers</h2>
+   <Image
+   src={moreThan1}
+   alt="2908 real estate completes win win deals"
+   />
    <p className="text-center mb-2">
    At 2908 Real Estate, we go beyond just cash offers; our commitment to helping
     Utah home sellers extends to finding proven and creative solutions for your specific needs
    </p>
+   <Image
+   src={moreThan2}
+   alt="Fixer upper sold as-is to 2908 realestate"
+   />
    <p className="text-center mb-2">
    If an all-cash offer doesn't align with your goals,
     we have the expertise to explore alternative options that can ease your financial burdens.
@@ -46,6 +74,10 @@ export default function Home() {
    Our team is skilled at devising payment plans tailored to
    your situation, providing you with the potential for monthly profits.
    </p>
+   <Image
+   src={moreThan3}
+   alt="2908 real estate seller finaced deal"
+   />
    <p className="text-center mb-2">
    Our goal is to ensure that you achieve the best possible outcome,
    allowing you to sell your house while maximizing your returns and securing your peace of mind.
@@ -91,7 +123,7 @@ export default function Home() {
     take care of the rest, providing you with a hassle-free and profitable selling experience.
    </p>
    </section>
-
+</section>
    </section>
    </>
  
