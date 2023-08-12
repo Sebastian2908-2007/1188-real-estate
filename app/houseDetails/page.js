d8b432bfc7f0d07b2a967ecf14f32708d851365f
Sebabin <sebastiancrespin279@gmail.com>
2023-08-12 14:29:12 -0600

    fixed qualify hero path for img

--- app/houseDetails/page.js
+++ app/houseDetails/page.js
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState, useEffect } from 'react';
-import VideoPlayer from './VideoPlayer';
+import VideoPlayer from '@/components/VideoPlayer';
 
 const HouseDetailsPage = ({ houseInfoId }) => {
   const [videoUrl, setVideoUrl] = useState('');
