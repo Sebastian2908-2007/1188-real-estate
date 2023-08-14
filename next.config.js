ad7c3ae56907bcab26e750cb1abbaa44022a5dd2
Sebabin <sebastiancrespin279@gmail.com>
2023-08-14 13:04:58 -0600

    added with pwa to next.config file everything seems to be working at this point

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
-const withVideos = require('next-videos')
+const withVideos = require('next-videos');
+const withPWA = require('next-pwa');
 const nextConfig = {}
 
-module.exports = withVideos(nextConfig)
+module.exports = withVideos(
+    withPWA({
+      pwa:{
+        dest:'public',
+        register: true,
+        skipWaiting: true,
+        disable: process.env.NODE_ENV === 'development'
+      } 
+    }),
+    nextConfig
+    )
+
+
+
+
+
